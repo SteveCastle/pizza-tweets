1b320e2cc259ce77ea3395ae5ffe194e56d90be0
Steve Castle <steve@tigerhaunt.com>
2014-10-03 21:05:58 -0700

    changed bounding box for feed

--- app/service/tweetfeed.js
+++ app/service/tweetfeed.js
@@ -3,10 +3,10 @@ var twitter = require('ntwitter'),
   credentials = require('../../config/credentials.js');
 
 var t = new twitter({
-  consumer_key: credentials.consumer_key,
-  consumer_secret: credentials.consumer_secret,
-  access_token_key: credentials.access_token_key,
-  access_token_secret: credentials.access_token_secret
+    consumer_key: credentials.consumer_key,
+    consumer_secret: credentials.consumer_secret,
+    access_token_key: credentials.access_token_key,
+    access_token_secret: credentials.access_token_secret
 });
 
 var mongo = require('mongodb');
@@ -18,21 +18,20 @@ var Server = mongo.Server,
 
 var server = new Server('localhost', 27017, {
     auto_reconnect: true
-  }),
+}),
   db = new Db('stream', server);
 // open db
 db.open(function (err, db) {
-  assert.equal(null, err);
-
-  t.stream('statuses/filter', {
-      'locations': '-122.75,36.8,-121.75,37.8,-74,40,-73,41'
+    assert.equal(null, err);
+    t.stream('statuses/filter', {
+        'locations': '-115.384091, 36.129459,-115.062159,36.336251'
     },
     function (stream) {
-      stream.on('data', function (tweet) {
-        db.collection('tweets', function (err, collection) {
-          collection.insert(tweet);
-          console.log(tweet);
+        stream.on('data', function (tweet) {
+            db.collection('tweets', function (err, collection) {
+                collection.insert(tweet);
+                console.log(tweet.user.screen_name,'in ',tweet.place.name, 'says ',tweet.text);
+            });
         });
-      });
-    })
+    });
 });
